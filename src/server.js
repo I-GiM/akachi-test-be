dbf87ba25738bd9c1dc98540098371c349fdb66a
I-GiM <ikennaoyiih@gmail.com>
2021-06-27 14:52:39 +0100

    add cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import routes from './apis/routes'
+import cors from 'cors'
 import './config/db'
 
 dotenv.config()
@@ -9,6 +10,7 @@ const app = express()
 const port = process.env.PORT
 
 app.use(express.json())
+app.use(cors())
 routes(app)
 
 app.listen(port, () => {
